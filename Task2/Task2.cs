60074a980e403ab045e9db0ff8a44ba235766978
DrozdovDan <daniil.drozdovjr@gmail.com>
2022-10-07 17:17:47 +0300

    Did task2

--- Task2/Task2.cs
+++ Task2/Task2.cs
@@ -1,3 +1,5 @@
+using static System.Text.Encoding;
+
 namespace Task2
 {
     public class Task2
@@ -5,7 +7,15 @@ namespace Task2
        
         public static void Main(string[] args)
         {
-            throw new NotImplementedException();
+            var fileName = args[0];
+            var e1 = GetEncoding(args[1]);
+            var e2 = GetEncoding(args[2]);
+            StreamReader w1 = new StreamReader(fileName, e1);
+            var text = w1.ReadToEnd();
+            w1.Close();
+            StreamWriter w2 = new StreamWriter(fileName, false, e2);
+            w2.Write(text);
+            w2.Close();
         }
     }
 }
--- Task2/Task2Test.cs
+++ Task2/Task2Test.cs
@@ -2,6 +2,7 @@ using System.Text;
 using NUnit.Framework;
 using static NUnit.Framework.Assert;
 using static Task2.Task2;
+using static System.Text.Encoding;
 
 namespace Task2;
 
@@ -13,9 +14,9 @@ public class Tests
         var tmpFileName = Path.GetTempFileName();
         try
         {
-            File.Copy("../../../data/text-utf8.txt", tmpFileName, true);
-            Main(new[] { tmpFileName, "utf-8", "window  s-1251" });
-            That(File.ReadAllBytes(tmpFileName), Is.EqualTo(File.ReadAllBytes("../../../data/text-windows-1251.txt")));
+            File.Copy("C:/Users/Admin/RiderProjects/Class4/Task2/text-utf-8.txt", tmpFileName, true);
+            Main(new[] { tmpFileName, "utf-8", "UTF-16LE" });
+            That(File.ReadAllBytes(tmpFileName), Is.EqualTo(File.ReadAllBytes("C:/Users/Admin/RiderProjects/Class4/Task2/text-utf-16.txt")));
         }
         finally
         {
@@ -29,14 +30,13 @@ public class Tests
         var tmpFileName = Path.GetTempFileName();
         try
         {
-            File.Copy("../../../data/text-windows-1251.txt", tmpFileName, true);
-            Main(new[] { tmpFileName, "windows-1251", "utf-8"});
-            That(File.ReadAllBytes(tmpFileName), Is.EqualTo(File.ReadAllBytes("../../../data/text-utf8.txt")));
+            File.Copy("C:/Users/Admin/RiderProjects/Class4/Task2/text-utf-16.txt", tmpFileName, true);
+            Main(new[] { tmpFileName, "UTF-16LE", "utf-8"});
+            That(File.ReadAllText(tmpFileName), Is.EqualTo(File.ReadAllText("C:/Users/Admin/RiderProjects/Class4/Task2/text-utf-8.txt")));
         }
         finally
         {
             File.Delete(tmpFileName);            
         }
     }
-
 }
